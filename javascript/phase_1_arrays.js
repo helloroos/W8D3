d21d3e7fd2cf2dc0e3db3d59568a731a1217dd14
Robby Balistreri <rbalistreri5@gmail.com>
2021-04-14 13:04:48 -0500

    Inital work on Phase I

---
+++ javascript/phase_1_arrays.js
@@ -0,0 +1,27 @@
+// Array.prototype.my_uniq = function() {
+//     let new_arr = [];
+//     for (let i = 0; i < this.length; i++){
+//         // this at i
+//         if (!new_arr.includes(this[i])){
+//             new_arr.push(this[i]);
+//         }
+//     }
+//     return new_arr;
+// }
+
+// console.log([1, 2, 2, 3, 3, 3].my_uniq())
+
+Array.prototype.twoSum = function() {
+    let new_array = [];
+    for (let i = 0; i < this.length; i++){
+        for (let j = i + 1; j < this.length; j++){
+            let pair = [this[i], this[j]]
+            if (pair[0] + pair[1] == 0 ){
+                new_array.push(pair);
+            }
+        }
+    }
+    return new_array;
+}
+
+console.log([1, -1, 5, 6, -4, -6].twoSum())
