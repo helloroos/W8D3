4933eb6f2aaf548ab70b0565c3633f915f91b4b8
michelleroos <hello.michelleroos@gmail.com>
2021-04-14 15:41:18 -0400

    Worked on myReduce

--- javascript/phase_2_enumerable.js
+++ javascript/phase_2_enumerable.js
@@ -30,25 +30,60 @@ Array.prototype.myEach = function(callback1) {
 
 // console.log([1,2,3,4].myMap(multiply))
 
-Array.prototype.myReduce = function(callback, [initialValue]){
+Array.prototype.myReduce = function(callback3, initialValue){
     let startValue;
     let testValue;
     if (initialValue){
         startValue = initialValue;
         testValue = this;
-    }
-    else {
+    } else {
         startValue = this[0];
-        testValue = this.slice(1)
-    }
+        testValue = this.slice(1);
+    };
+    testValue.myEach(function(x) { 
+        startValue = callback3(startValue, x);
+    })
+    return startValue;
+}
+
+function summer(acc, el) {
+    return acc + el;
 }
 
 // without initialValue
-[1, 2, 3].myReduce(function (acc, el) {
-    return acc + el;
-}); // => 6
+console.log([1, 2, 3].myReduce(summer, 25)); // => 6
 
-// with initialValue
-[1, 2, 3].myReduce(function (acc, el) {
-    return acc + el;
-}, 25); // => 31
+// // with initialValue
+// [1, 2, 3].myReduce(function (acc, el) {
+//     return acc + el;
+// }, 25); // => 31
+
+
+// def my_inject(accumulator = nil, & prc)
+//     arr = self
+
+//     if accumulator.nil ?
+//         accumulator = self.first
+//         arr = self.drop(1)
+//     end
+
+//     arr.each do | el |
+//         accumulator = prc.call(accumulator, el)
+//         end
+
+//     accumulator
+// end
+
+// Array.prototype.myReduce = function(callback3, [initialValue]){
+//     let arr = this;
+
+//     if (initialValue == null) {
+//         let initialValue = arr[0];
+//         let el = arr.slice(1);
+//     }
+
+//     arr.myEach(function(x) {
+//     })
+
+//     return acc;
+// }
